43546d5f95fe81020b45cedeb08f7a42f31a7394
Magniez Loick <loick_magniez@ens.univ-artois.fr>
2016-10-29 16:54:40 +0200

    Fonction afficherElt terminées

--- projet.sh
+++ projet.sh
@@ -1,23 +1,32 @@
 #! /bin/bash
 
-function afficherElt()
+#Retourne la liste complete des fichiers (NON RECURSIF)
+function afficherElts()
 {	
 	OLD_IFS=$IFS
-	res=`find . -maxdepth 1 -name "*" | awk '{print $0":"}'`
-	echo $res
-	
-	IFS=":"
-	for i in $res
-	do
-		echo $i
-	done
-	
-	IFS=$OLD_IFS
-	
-	
-	#sed -n 3p 
+	res=`find $1 -maxdepth 1 -name "*"`	
+	res=`echo "$res" | tr "\n$" ":"` #remplace saut de ligne par :
 	
 }
+
+#Affiche le nom d'un element en particulier (Par défaut: SANS SON CHEMIN)
+#3 arguments: Affiche le chemin complet
+function afficherElt()
+{
+	afficherElts $1
+	
+	
+	res=`echo "$res"|cut -d: -f$2` #recupere l'element en question
+	if (test $# -eq 3)
+	then
+		res=`echo "$res"|sed "s/.*\\///g"` #coupe le chemin 
+	fi
+	echo "$res"
+	
+}
+
+	
+
 
 #ls -l | awk '{print $1":"$2":"$3}'
 
@@ -31,14 +40,8 @@ function afficherElt()
 #afficherElt 4
 
 
-for i in `find . -maxdepth 1 -name "*"`
-do
-	echo $i
-done
+afficherElts $1
+afficherElt $1 5 1
+afficherElt $1 5
 
-for i in `ls "$1"`
-do
-	echo "ok"
-done 
-
-afficherElt
+#afficherEltChemin $1 5
