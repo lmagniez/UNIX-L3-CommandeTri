dad20302aae7cf7b4b09eff67bc545085c3fbf1f
Magniez Loick <loick_magniez@ens.univ-artois.fr>
2016-10-30 16:38:37 +0100

    Tri a Bulle et Comparaison Nom

--- projet.sh
+++ projet.sh
@@ -3,7 +3,7 @@
 #Retourne la liste complete des fichiers (NON RECURSIF)
 function afficherElts()
 {	
-	OLD_IFS=$IFS
+	
 	res=`find $1 -maxdepth 1 -name "*"`	
 	res=`echo "$res" | tr "\n$" ":"` #remplace saut de ligne par :
 	
@@ -15,26 +15,46 @@ function afficherElts()
 
 function afficherElt()
 {
-	
 	res=`echo "$1"|cut -d: -f$2` #recupere l'element en question
-	if (test $# -eq 3)
-	then
-		res=`echo "$res"|sed "s/.*\\///g"` #coupe le chemin 
-	fi
-	#echo "$res"
-	
 }
+
+#A partir d'un chemin complet, retourne le nom du fichier
+function getNom()
+{
+	res=`echo "$1"|sed "s/.*\\///g"`
+}
+
 
 #compare taille entre elt1 et elt2
 #OK si elt1>elt2 sinon KO
-#Nom sans chemin complet
+#Nom AVEC chemin complet
 function cmpNom()
 {
-	if (test $1 \> $2)
-		then echo OK
+	
+	getNom "$1"
+	nom1=$res
+	getNom "$2"
+	nom2=$res
+	
+	if (test "$nom1" \> "$nom2")
+		then res="OK"
 	else
-		echo KO
+		res="KO"
 	fi
+}
+
+#taille de la liste
+function len()
+{
+	OLD_IFS=$IFS
+	IFS=':'
+	cpt=0
+	for i in $1
+	do
+		cpt=`expr $cpt + 1`
+	done
+	IFS=$OLD_IFS
+	res=$cpt
 }
 
 #Switch l'elt1 avec l'elt2 dans la liste
@@ -44,8 +64,9 @@ function cmpNom()
 function switchElt()
 {
 	
+	
 	#i1 DOIT ETRE INFERIEUR a i2
-	if (test $2 \> $3)
+	if (test $2 -gt $3)
 	then
 		i1=$3
 		i2=$2
@@ -56,17 +77,72 @@ function switchElt()
 	
 	
 	afficherElt "$1" $i1
-	elt1=$res
+	elt1="$res"
 	afficherElt "$1" $i2
-	elt2=$res
-	
-	echo "elt 1: $elt1"
-	echo "elt 2: $elt2"
+	elt2="$res"
 	
 	res="$1"
-	res=`echo "$res"|sed "s#$elt2#$elt1#"` #doit remplacer delimiteur car des slash dans la variable
-	res=`echo "$res"|sed "s#$elt1#$elt2#"`
-	echo "$res" 
+	res=`echo "$res"|sed "s#$elt2:#$elt1:#"` #doit remplacer delimiteur / par # car / present dans la variable 
+	res=`echo "$res"|sed "s#$elt1:#$elt2:#"`
+	
+}
+
+#Prend une liste en parametre
+#$1: liste	$2: nom fonction comparaison
+function triBulle()
+{
+	liste=$1
+	len "$liste"
+	
+	long=$res
+	
+	#tant qu'on a plus de deux elts
+	while [ $long -gt 2 ] 
+	do
+		
+		i=2
+		#parcoure les elt et switch pour emmener le max a la fin
+		while [ $i -lt $long ]
+		do
+			afficherElt "$liste" $i
+			elt1="$res"
+			afficherElt "$liste" `expr $i + 1` 
+			elt2="$res"
+			
+			
+			$2 "$elt1" "$elt2"
+			
+			
+			if (test "$res" = "OK")
+			then
+				switchElt "$liste" $i `expr $i + 1` 
+				liste="$res"
+			fi
+			
+			
+			
+			
+			
+			i=`expr $i + 1`
+		done
+	    long=`expr $long - 1`
+	done
+	
+	afficherNoms "$liste"
+		
+	
+	
+}
+
+function afficherNoms()
+{
+	OLD_IFS=$IFS
+	IFS=":"
+	for i in $1
+	{
+		getNom $i
+		echo $res
+	}
 }
 
 
@@ -83,11 +159,19 @@ liste=$res
 #afficherElt "$liste" 5 1
 #afficherElt "$liste" 2
 
-echo "$liste"
+#echo "$liste"
+
+#len "$liste"
+
+#cmpNom "test2" "test" 
+
+#switchElt "$liste" 3 4
+
+#switchElt "$liste" 9 11
 
 
 
-switchElt "$liste" 2 3
 
+triBulle "$liste" "cmpNom"
 
 #afficherEltChemin $1 5
