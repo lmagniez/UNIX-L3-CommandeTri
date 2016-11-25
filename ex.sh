439a1a37aaf8883f9ee7f843153057b9dc600c96
Magniez Loick <loick_magniez@ens.univ-artois.fr>
2016-11-25 22:42:42 +0100

    implémentation tri fusion

--- ex.sh
+++ ex.sh
@@ -1,3 +1,18 @@
-#!/bin/bash
+#!/bin/sh
 #vincent valembois - loick magniez
+
+
+tableau=( blanc noir rouge bleu vert jaune )
+
+echo ${tableau[5]}
+
+
+echo ${#tableau[*]}
+
+
+tableau[6]=turquoise
+echo ${#tableau[*]}
+
+
+echo ${tableau[6]}
 
--- projet.sh
+++ projet.sh
@@ -14,7 +14,8 @@ function afficherElts()
 #3 arguments: Affiche le nom sans son chemin
 function afficherElt()
 {
-	res=`echo "$1"|cut -d: -f$2` #recupere l'element en question
+	t=`expr $2 + 1`
+	res=`echo "$1"|cut -d: -f$t` #recupere l'element en question
 }
 
 #A partir d'un chemin complet, retourne le nom du fichier
@@ -89,7 +90,6 @@ function len()
 function switchElt()
 {
 	
-	
 	#i1 DOIT ETRE INFERIEUR a i2
 	if (test $2 -gt $3)
 	then
@@ -106,9 +106,27 @@ function switchElt()
 	afficherElt "$1" $i2
 	elt2="$res"
 	
+	
 	res="$1"
-	res=`echo "$res"|sed "s#$elt2:#$elt1:#"` #doit remplacer delimiteur / par # car / present dans la variable 
-	res=`echo "$res"|sed "s#$elt1:#$elt2:#"`
+	res=`echo "$res"|sed "s|$elt2:|$elt1:|"` #doit remplacer delimiteur / par | car / present dans la variable 
+	res=`echo "$res"|sed "s|$elt1:|$elt2:|"`
+	
+}
+
+#Remplace l'elt d'indice i d'une liste
+#$1: liste $2:i $3:elt
+function changeElt()
+{
+	
+	elt2=$3
+	
+	i=`expr $2 + 1` #on accede au ième element (indice+1)
+	afficherElt "$1" $2
+	elt1="$res"
+	
+	res="$1"
+	res=`echo "$res" | sed "s|[^:]*|$elt2|$i"`
+	
 	
 }
 
@@ -125,16 +143,17 @@ function triBulle()
 	while [ $long -gt 2 ] 
 	do
 		
-		i=2
+		i=1 #2
 		#parcoure les elt et switch pour emmener le max a la fin
-		while [ $i -lt $long ]
+		while [ $i -lt `expr $long - 1` ]
+		#while [ $i -lt $long ]
 		do
 			afficherElt "$liste" $i
 			elt1="$res"
 			afficherElt "$liste" `expr $i + 1` 
 			elt2="$res"
 			
-			
+			#fonction de comparaison
 			$2 "$elt1" "$elt2"
 			
 			
@@ -143,11 +162,7 @@ function triBulle()
 				switchElt "$liste" $i `expr $i + 1` 
 				liste="$res"
 			fi
-			
-			
-			
-			
-			
+				
 			i=`expr $i + 1`
 		done
 	    long=`expr $long - 1`
@@ -158,6 +173,117 @@ function triBulle()
 	
 	
 }
+
+#$1:liste $2:cmpFonc
+function tri_fusion()
+{
+	len "$1"	
+	long=$res
+	
+	if [ $long -gt 0 ]
+	then
+		tri_fusion_bis "$1" "$2" 1 `expr $long - 1`
+		liste="$res"
+	fi
+	
+	afficherNoms "$liste"
+	res="$liste"
+}
+
+#$1:liste $2:cmpFonc $3:deb  $4:fin 
+function tri_fusion_bis()
+{	
+	
+	
+	liste=$1
+	deb=$3
+	fin=$4
+	if [ $deb -ne $fin ]
+	then
+	
+		#partition1
+		milieu=`expr \( $4 + $3 \) / 2`
+		tri_fusion_bis "$liste" "$2" $3 $milieu #deb milieu
+		liste="$res"
+		
+		#partition2
+		milieu=`expr \( $4 + $3 \) / 2` #doit recalculer (SINON utilise milieu de fonction rec)
+		tri_fusion_bis "$liste" "$2" `expr $milieu + 1` $4 #milieu+1 fin
+		liste="$res"
+		
+		#fusion
+		milieu=`expr \( $4 + $3 \) / 2` #doit recalculer (SINON utilise milieu de fonction rec)
+		fusion "$liste" "$2" $3 $milieu $4 #fusion deb milieu fin
+		liste="$res"
+	fi
+	
+	#afficherNoms "$liste"
+	
+	res="$liste"
+	
+}
+
+#$1:liste $2:cmpFonc $3:deb $4:milieu $5:fin 
+function fusion()
+{
+	deb=$3
+	deb2=`expr $milieu + 1` 
+	milieu=$4
+	fin=$5
+	cpt1=$deb
+	cpt2=`expr $milieu + 1`
+	
+	liste="$1" #utilise pour le resultat
+	liste_copie="$1" #copie pour faire le tri
+	
+	for i in $(seq $deb $fin)
+	do
+		#tous les elts du premier tab utilisés
+		if [ $cpt1 -eq $deb2 ]
+		then
+			break #tous les elts classés
+		
+		#tous les elts du second tab utilisés
+		elif [ $cpt2 -eq `expr $fin + 1` ]
+		then
+			afficherElt "$liste_copie" $cpt1
+			elt="$res"
+			changeElt "$liste" $i "$elt"
+			liste="$res"
+			cpt1=`expr $cpt1 + 1`
+		
+		#comparaison 2 elts
+		else			
+			afficherElt "$liste_copie" $cpt1
+			elt1="$res"
+			afficherElt "$liste" $cpt2
+			elt2="$res"
+			 
+			#cmp e1 e2
+			$2 "$elt1" "$elt2"
+			
+			#tab[cp1]<tab[cpt2]?
+			if (test "$res" = "KO")
+			then
+				#KO: true Change Elt1
+				changeElt "$liste" $i "$elt1" 
+				liste="$res"
+				cpt1=`expr $cpt1 + 1`
+			else
+				#OK: false Change Elt2
+				changeElt "$liste" $i "$elt2" 
+				liste="$res"
+				cpt2=`expr $cpt2 + 1`
+			fi
+		fi
+	done
+	
+	res="$liste"
+	
+}
+
+
+
 
 function afficherNoms()
 {
@@ -185,8 +311,12 @@ function afficherNoms()
 
 afficherElts $1
 liste=$res
-#afficherElt "$liste" 5 1
-#afficherElt "$liste" 2
+
+#echo $liste
+#afficherElt "$liste" 1
+#echo "1: "$res
+
+afficherElt "$liste" 2
 
 #echo "$liste"
 
@@ -203,13 +333,28 @@ getTaille ~/Bureau/TDSHELL/
 getTaille ./projet.sh
 
 cmpTaille ./projet.sh ~/Bureau/TDSHELL/ 
+cmpNom	./.git ./README.md
 echo $res
 
+echo "
+
+
+noms"
+
+#afficherNoms "$liste"
+
 echo ">>>>>>>>TRI NOM"
-triBulle "$liste" "cmpNom"
+
+#tri_fusion "$liste" "cmpNom"
+
+echo "BULLE"
+#triBulle "$liste" "cmpNom"
+
+
 
 
 echo ">>>>>>>>TRI TAILLE"
-triBulle "$liste" "cmpTaille"
+#triBulle "$liste" "cmpTaille"
+
 
 #afficherEltChemin $1 5
